00a2695edcf23ef76e9733cd2cbb13a915a9ff02
Atis <atis@nyantech.com>
2021-12-07 15:50:00 +0900

    main をリファクタした

--- main.go
+++ main.go
@@ -6,28 +6,43 @@ import (
 	"os/signal"
 	"syscall"
 
-	argos_handlers "atis.dev/argos/handlers"
+	"atis.dev/argos/handlers"
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func init() {
 	godotenv.Load(".env")
+}
 
-	client, err := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+func main() {
+	token := os.Getenv("DISCORD_TOKEN")
+	client := getClient(token)
+
+	handlers.AddHandlers(client)
+	openClient(client)
+	runClient(client)
+}
+
+func getClient(token string) *discordgo.Session {
+	client, err := discordgo.New("Bot " + token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
-		return
+		return nil
 	}
 
-	argos_handlers.AddHandlers(client)
+	return client
+}
 
-	err = client.Open()
+func openClient(client *discordgo.Session) {
+	err := client.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+}
 
+func runClient(client *discordgo.Session) {
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
